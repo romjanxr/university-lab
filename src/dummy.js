8dd013f355800966471371cfefacb84aa46cf5f5
romjanxr <romjanvr5@gmail.com>
2021-10-04 13:57:23 +0600

    first commit

---
+++ src/dummy.js
@@ -0,0 +1,42 @@
+const dummy = [
+    {
+        id: 1,
+        title: 'Best Certificate',
+        description: 'Our University has the best certificate. Every year thousands of students get excellente certificate from different programme. We perticipate in national and international programme.'
+    },
+    {
+        id: 2,
+        title: 'National Awards',
+        description: 'Our University faculty receive frequent national and international recognition for their outstanding contributions to their fields.'
+    },
+    {
+        id: 3,
+        title: 'Qualifier Teacher',
+        description: 'Our Teachers Teach students through lecture, tutorial, discussion, seminar, practical demonstration and workshop. Make personal communication with the students.'
+    },
+    {
+        id: 4,
+        title: 'Dedicated Courses',
+        description: 'We have so many dedicated courses and faculties. You can choose your perfect university program with our course guides – covering entry requirements, specializations, career prospects and more'
+    },
+    {
+        id: 5,
+        title: 'Career Services',
+        description: 'Offers quality career and internship resources to help students and alumni identify and fulfill their career goals.'
+    },
+    {
+        id: 6,
+        title: 'Over 4 k Students',
+        description: 'More than four thousands students choose our university.We enforce to enhance the teacher-student relationship, liaise with other universities, organizations and student bodies at national and international level.'
+    },
+    {
+        id: 7,
+        title: '24 H Support',
+        description: 'We have Student Support, In Student Support Whoever you are and wherever you come from we are committed to providing the best possible support to all of our students.'
+    },
+    {
+        id: 8,
+        title: 'Student Wellnes',
+        description: 'In our University, we believe that holistic wellness is integral to student learning and success. Wellness is a practice that’s cultivated over time.'
+    },
+]
